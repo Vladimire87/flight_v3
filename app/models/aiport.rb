07fa2707c9d3d3e108048acd1d34a6c71226deb1
Vladimir Egorov <wahaplay@gmail.com>
2024-07-25 13:10:43 +0300

    db migrate

--- app/models/aiport.rb
+++ app/models/aiport.rb
@@ -1,2 +1,15 @@
+# == Schema Information
+#
+# Table name: aiports
+#
+#  id         :integer          not null, primary key
+#  code       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_aiports_on_code  (code) UNIQUE
+#
 class Aiport < ApplicationRecord
 end
