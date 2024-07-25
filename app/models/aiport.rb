a1d580d921c1ead893321bc5c8dd7fbfdb252773
Vladimir Egorov <wahaplay@gmail.com>
2024-07-25 13:02:13 +0300

    Create Airport model

---
+++ app/models/aiport.rb
@@ -0,0 +1,2 @@
+class Aiport < ApplicationRecord
+end
---
+++ db/migrate/20240725095903_create_aiports.rb
@@ -0,0 +1,10 @@
+class CreateAiports < ActiveRecord::Migration[7.1]
+  def change
+    create_table :aiports do |t|
+      t.string :code
+
+      t.timestamps
+    end
+    add_index :aiports, :code, unique: true
+  end
+end
