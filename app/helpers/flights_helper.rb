a4b1fc83f2416bcae30ac423138d26ed9c1a5535
Vladimir Egorov <wahaplay@gmail.com>
2024-07-27 14:34:55 +0300

    add helper for min to hours for flight duration

--- app/helpers/flights_helper.rb
+++ app/helpers/flights_helper.rb
@@ -1,9 +1,15 @@
 # frozen_string_literal: true
 
 module FlightsHelper
-  def min_to_hours(min)
-    hours = min / 60
-    minutes = min % 60
-    "#{hours} час#{'а' if hours != 1} #{minutes} минут#{'ы' if minutes != 1}"
+  def min_to_hours(mins)
+    hours, minutes = mins.divmod 60
+    if minutes < 10
+      minutes = minutes.to_s.prepend '0'
+    end
+    if hours < 1
+      "Travel time: #{minutes}m"
+    else
+      "Travel time: #{hours}h #{minutes}m"
+    end
   end
 end
